a71497eef7f3369f722bede0955b202a47736ad9
HungVM2 <lolo.aquarius@gmail.com>
2019-03-06 21:46:28 +0700

    Add something

--- record.sh
+++ record.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #sudo avconv -y -r 1 -f video4linux2 -input_format mjpeg -i /dev/video0 -codec copy -r 24 /media/pi/BAOO/output.mpeg
-sudo avconv -y -f video4linux2 -video_size hd720 -i /dev/video0 -qscale 1 /media/pi/BAOO/output.avi
-#python3 /home/pi/Desktop/face_cascade.py --video /media/pi/BAOO/output.avi
-#-input_format mjpeg -c:v copy
+#sudo avconv -y -f video4linux2 -video_size hd1080 -i /dev/video1 -r 24 ~/Desktop/output.avi
 #ffmpeg -f v4l2 -framerate 30 -video_size 1920x1080 -c:v libx264 -i /dev/video0 -c:v copy output.avi
+#ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 output.mkv
+
+## stream copy first
+#ffmpeg -f v4l2 -framerate 18 -video_size 1920x1080 -input_format mjpeg -i /dev/video1 -c copy -an ~/Desktop/output.mkv
+## re encode later
+#ffmpeg -i mjpeg.mkv -c:v libx264 -crf 23 -preset medium -pix_fmt yuv420p out.mkv
+ffmpeg -i ~/Desktop/output.mkv -s 1920x1080 -c:v libx264 -preset slower -crf 17 -c:a copy -y ~/Deskotp/out.mkv -async 1 -vsync 1
+
+#ffmpeg -f v4l2 -framerate 90 -video_size 1920x1080 -input_format mjpeg -i /dev/video1 output.mkv
--- timestamp.py
+++ timestamp.py
@@ -1,29 +1,39 @@
 import cv2
 import datetime
 import numpy as np
+import time
 
 stream = cv2.VideoCapture(1)
-stream.set(3,1280)
-stream.set(4,1024)
+stream.set(3,1920)
+stream.set(4,1080)
 #fps_cam = cv2.CAP_PROP_FPS
 #stream.set(5,30.0)
 # Define the codec and create VideoWriter object to save the video
 fourcc = cv2.VideoWriter_fourcc(*'XVID') 
-video_writer = cv2.VideoWriter('Desktop/output.avi', fourcc, 11, (1280, 720))
+video_writer = cv2.VideoWriter('Desktop/output.avi', fourcc, 24, (1920, 1080))
 
 font = cv2.FONT_HERSHEY_COMPLEX
 
-
+frame_count = 0
+start_time = time.time()
+time_interval = 3
+frame_rate = 0
 while True:
+        end_time = time.time()
         success, frame = stream.read()
         if not success:
             break        
+        if (end_time - start_time) > time_interval:
+            frame_rate = int(frame_count / (end_time - start_time))
+            start_time = time.time()
+            frame_count = 0
         frame = cv2.flip(frame, 1)
         now = datetime.datetime.now()
         cv2.putText(frame, str(now),(0, 50), font, 1,(0,255,0), 2, cv2.LINE_AA)
+        cv2.putText(frame, 'Fps :' + str(frame_rate),(0, 100), font, 1,(0,255,0), 2, cv2.LINE_AA)
         cv2.imshow("frame", frame)
         video_writer.write(frame)
-        
+        frame_count += 1
         key = cv2.waitKey(1) & 0xFF
         if key==ord('q'):
             break
